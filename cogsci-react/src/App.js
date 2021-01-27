f2345351207540ad4c8e44535c89d373ef2176cf
Kirito-chan <fero.kochjar@gmail.com>
2021-01-27 15:03:04 +0100

    vymazal link

--- cogsci-react/src/App.js
+++ cogsci-react/src/App.js
@@ -41,14 +41,6 @@ function App() {
             Redux Toolkit
           </a>
           ,<span> and </span>
-          <a
-            className="App-link"
-            href="https://react-redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React Redux
-          </a>
         </span>
       </header>
     </div>
