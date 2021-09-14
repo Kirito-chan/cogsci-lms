4c645e050a059560b706d5ec7defed4283fd843b
frantisek.kochjar <frantisek.kochjar@atos.net>
2021-09-14 22:23:14 +0200

    preklep v prefixe pre base URL

--- client/src/constants.js
+++ client/src/constants.js
@@ -11,7 +11,7 @@ export const USER_LOGOUT = "USER_LOGOUT";
 export const MAX_POINT_HEIGHT_PRES_EVALUATION = 10;
 
 // URL pre router
-export const URL_PREFIX = "/prezenacie"
+export const URL_PREFIX = "/prezentacie"
 
 export const URL_HOME = "/home-student";
 export const URL_SUBJECTS = "/subjects";
