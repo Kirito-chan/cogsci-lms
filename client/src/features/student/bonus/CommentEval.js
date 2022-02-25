ad763427196b0997a5751f46a9947ce57d23db86
Kochjar <frantisek.kochjar@atos.net>
2022-02-25 21:23:32 +0100

    fixol zobrazenie zmeny pridania/odobrania bodu pri bonusovej ulohe, kedze to neukazovalo pokym sa nerefreshla stranka, lebo do Reduxu sa to neulozilo

--- client/src/features/student/bonus/CommentEval.js
+++ client/src/features/student/bonus/CommentEval.js
@@ -1,16 +1,18 @@
 import React, { useState } from "react";
 import Button from "react-bootstrap/Button";
 import { useDispatch } from "react-redux";
+import { useParams } from "react-router";
 import LoadingInButton from "../../../components/LoadingInButton";
 import {
   GOT_0_BONUS_POINTS,
   GOT_1_BONUS_POINTS,
   NOT_YET_EVALUATED_BONUS_POINTS,
 } from "../../../constants";
-import { updateBonusValuated } from "./bonusSlice";
+import { loadComments, updateBonusValuated } from "./bonusSlice";
 import "./CommentEval.css";
 
 function CommentEval({ bonusId, comment }) {
+  const { subjectId } = useParams();
   const dispatch = useDispatch();
   const [loadingZero, setLoadingZero] = useState(false); // loading for zero points button
   const [loadingOne, setLoadingOne] = useState(false);
@@ -44,12 +46,14 @@ function CommentEval({ bonusId, comment }) {
         setLoading(false);
         setSameButtonActive(false);
         setTextDisplayed("d-inline-block");
+        if (bonusId && subjectId) dispatch(loadComments(bonusId, subjectId));
       });
     } else {
       dispatch(updateBonusValuated(bonusId, comment.id, valuated)).then(() => {
         setLoading(false);
         setSameButtonActive(true);
         setTextDisplayed("d-none");
+        if (bonusId && subjectId) dispatch(loadComments(bonusId, subjectId));
       });
     }
   };
